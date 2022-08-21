27176b22849b12fab21954cb8b1d7e94e056df6a
liuqi <liuqi@email.jingqb.com>
2022-08-21 14:52:15 +0800

    first commit

--- src/main/java/com/kk/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/kk/demo/service/impl/DemoServiceImpl.java
@@ -8,7 +8,7 @@ public class DemoServiceImpl implements IDemoService {
 
     @Override
     public String get(String name) {
-        System.out.println("service 实现类中的name参数：" + name) ;
+        System.out.println("service impl is name = " + name) ;
         return name;
     }
 
